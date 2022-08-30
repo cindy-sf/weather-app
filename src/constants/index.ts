5a3a1cd09ed83be340dd7cf22413145f82c39801
cindy-sf <c.saintfleurant@gmail.com>
2022-08-30 18:23:46 +0200

    feat: adding weatherDetailsInfos to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,30 @@ export const API_BASE_URL = 'https://api.openweathermap.org/data/2.5'
 export const CITY = 'Paris'
 export const DEFAUL_LANG = 'fr'
 export const UNITS = 'metric'
+
+export const weatherDetailsInfos = [
+  {
+    name: 'Vent',
+    icon: 'wind.png',
+    dataPath: 'wind.speed',
+    unit: 'km/h',
+  },
+  {
+    name: 'Ressenti',
+    icon: 'termometer.png',
+    dataPath: 'main.feels_like',
+    unit: '°',
+  },
+  {
+    name: 'Humidité',
+    icon: 'humidity.png',
+    dataPath: 'main.feels_like',
+    unit: '%',
+  },
+  {
+    name: 'Pression',
+    icon: 'pressure.png',
+    dataPath: 'main.pressure',
+    unit: 'hPa',
+  },
+]
